436ed18433f5a7b3e76452b69fd6abf16b37cabe
Richi <ricaldo2431@gmail.com>
2022-07-15 15:39:23 -0500

    primer commit, la app consulta correctemnete devices del api sigfox

---
+++ api/v1/index.js
@@ -0,0 +1,132 @@
+const fetch = require("node-fetch");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(
+    express.json({
+        type: "*/*",
+    })
+);
+
+app.use(cors());
+
+console.clear();
+
+//consulta devices al api sigfox
+async function getDevicesFromSigfoxApi(user, pss, groupId, devTypeId) {
+    console.log(`deviceTypeid = ${devTypeId}`);
+    const auth = `${user}:${pss}`;
+    const authCoded = Buffer.from(auth, "utf-8").toString("base64");
+    // const authCoded = btoa(`${formusername}:${formpassword}`); //funciona pero btoa es obsoleta
+
+    const url = `https://api.sigfox.com/v2/devices?deviceTypeId=${devTypeId}`;
+    const options = {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "no-cors", // no-cors, *cors, same-origin
+        credentials: "include", // include, *same-origin, omit
+        headers: {
+            Authorization: `basic ${authCoded}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.ok) {
+            console.log("DATOS DE LA SOLICITUD A SIGFOX");
+            console.log("fetch ok");
+            console.log("respuesta server ok");
+            console.log(`codigo de respuesta: ${response.status}`);
+            console.log("url: ", response.url);
+
+            const responseJson = await response.json();
+
+            console.log("PARSEO JSON DATA CORRECTO");
+            // console.log(responseJson);
+            return responseJson;
+        } else {
+            console.log("RESPUESTA SERVER ERROR");
+            console.log(`RESPUESTA CODE: ${response.status}`);
+            // console.log("RESPUESTA DATA: ");
+            // console.log(response);
+        }
+    } catch (error) {
+        console.log("ERROR HACIENDO ALGO:");
+        console.log(error);
+    }
+}
+
+//cuando hagan un get a http://localhost:3000/devices hacer esto
+app.get("/devices", (req, res) => {
+    console.log(`SE RECIBE UN REQUEST DESDE EL FRONTEND`);
+
+    //Obteniendo headers del request del frontend
+    const hostname = req.hostname;
+    const user = req.headers.user;
+    const password = req.headers.password;
+    const groupId = req.headers.groupid;
+    const deviceTypeId = req.headers.devicetypeid;
+
+    console.log(`INFORMACION DEL REQUEST:`);
+    console.log(`api: request get desde http://${hostname} `);
+    console.log(`api: header user: ${user}`);
+    console.log(`api: header password: ${password}`);
+    console.log(`api: header groupId: ${groupId}`);
+    console.log(`api: header deviceTypeId: ${deviceTypeId}`);
+
+    // consultar API sigfox por devices
+    console.log("SE REALIZARÁ REQUEST A API SIGFOX");
+
+    const response = getDevicesFromSigfoxApi(
+        user,
+        password,
+        groupId,
+        deviceTypeId
+    );
+
+    response.then((info) => {
+        console.log("DATA RECIBIDA DESDE SIGFOX: ");
+        console.log(info);
+        res.send(info);
+
+        // // armar json para devolver al frontend
+        // toFront = {
+        //     data: [],
+        // };
+
+        // info.data.forEach((element) => {
+        //     toFront.data[info.data.indexOf(element)] = {
+        //         id: element.id,
+        //         unsubscriptionTime: "undefined",
+        //     };
+        // });
+
+        // // devolver listado de id´s
+        // console.log(`DATOS DEVUELTOS DESDE EL BACKEND UAPP`);
+        // console.log(toFront);
+        // res.send(toFront);
+    });
+});
+
+//cuando hagan un post a http://localhost:3000/unsubscribe hacer esto
+app.post("/unsub", (req, res) => {
+    console.log("api: me hicieron un request post");
+    res.send("api: en un momento respondo a su request post");
+    // armar json con id devices y unsubscriptionTime (doc api sigfox)
+    // hacer request post al API sigfox para poner unsubscription date a los devices
+});
+
+app.listen(port, () => {
+    console.log(`api: estoy ejecutandome en htttp://localhost:${port}`);
+});
---
+++ web/js/app.js
@@ -0,0 +1,176 @@
+const GetDevicesListForm = document.querySelector("#GetDevicesListForm");
+const username = document.querySelector("#username");
+const usernameError = document.querySelector("#usernameError");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError");
+const group = document.querySelector("#group");
+const groupError = document.querySelector("#groupError");
+const deviceType = document.querySelector("#deviceType");
+const deviceTypeError = document.querySelector("#deviceTypeError");
+const alertSuccess = document.querySelector(".alert-success");
+const alertDanger = document.querySelector(".alert-danger");
+const getDevicesButton = document.querySelector("#getDevicesButton");
+const unsubscribeButton = document.querySelector("#unsubscribeButton");
+const unsubscribeError = document.querySelector("#unsubscribeError");
+const devicesTable = document.querySelector("#devicesTable");
+const tableBody = document.querySelector("#devicesTableBody");
+const templateFila = document.querySelector("#templateFila").content;
+
+const loading = document.querySelector("#loading");
+
+const regexpUserName = /^[A-Za-z0-9\s]+$/;
+const regexpPassword = /^[A-Za-z0-9\s]+$/;
+const regexpGroup = /^[A-Za-z0-9\s]+$/;
+const regexpDeviceType = /^[A-Za-z0-9\s]+$/;
+
+//data recibida desde sigfox
+let sigfoxResponse = {};
+
+//funcion para consultar al backend los devices en un determiando device type
+async function getDevices(
+    formusername,
+    formpassword,
+    formgroup,
+    formdeviceType
+) {
+    const url = "http://localhost:3000/devices"; //para consultar devices en el device type especificado
+    const options = {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            user: formusername,
+            password: formpassword,
+            groupid: formgroup,
+            devicetypeid: formdeviceType,
+            accept: "application/json",
+            "content-Type": "application/json",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.ok) {
+            console.log("DATOS DE LA SOLICITUD A SIGFOX");
+            console.log("fetch ok");
+            console.log("respuesta server ok");
+            console.log(`codigo de respuesta: ${response.status}`);
+            console.log("url: ", response.url);
+
+            const responseJson = await response.json();
+
+            console.log("PARSEO JSON DATA CORRECTO");
+            // console.log(responseJson);
+            return responseJson;
+        } else {
+            console.log("RESPUESTA SERVER ERROR");
+            console.log(`RESPUESTA CODE: ${response.status}`);
+            // console.log("RESPUESTA DATA: ");
+            // console.log(response);
+        }
+    } catch (error) {
+        console.log("ERROR HACIENDO ALGO:");
+        console.log(error);
+    }
+}
+
+//mostrar/ocultar spinner de carga
+const loadingData = (estado) => {
+    if (estado) {
+        loading.classList.remove("d-none");
+        loading.classList.add("d-flex");
+    } else {
+        loading.classList.remove("d-flex");
+        loading.classList.add("d-none");
+    }
+};
+
+//pintar en la pagina la informacion recibida
+const pintarData = (response) => {
+    if (response.data.length > 0) {
+        console.log("pintando data recibida");
+        devicesTable.style.display = "block";
+        const fragment = document.createDocumentFragment();
+
+        //limpiar el body de la tabla
+        while (tableBody.childElementCount > 1) {
+            tableBody.removeChild(tableBody.lastChild);
+        }
+
+        response.data.forEach((item) => {
+            const clone = templateFila.cloneNode(true);
+            clone.querySelector(".idItem").textContent = item.id;
+            clone.querySelector(".dtItem").textContent = item.deviceType.id;
+            clone.querySelector(".grItem").textContent = item.group.id;
+            fragment.appendChild(clone);
+        });
+        tableBody.appendChild(fragment);
+    } else {
+        unsubscribeError.textContent = "No devices to unsubscribe!";
+        unsubscribeError.style.display = "block";
+    }
+};
+
+//cuando se hace click en el boton Get Devices
+getDevicesButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    loadingData(true);
+    usernameError.style.display = "none";
+    passwordError.style.display = "none";
+    groupError.style.display = "none";
+    deviceTypeError.style.display = "none";
+    unsubscribeError.style.display = "none";
+
+    let error = false;
+
+    if (!regexpUserName.test(username.value)) {
+        usernameError.style.display = "block";
+        error = true;
+    }
+    if (!regexpPassword.test(password.value)) {
+        passwordError.style.display = "block";
+        error = true;
+    }
+    if (!regexpGroup.test(group.value)) {
+        groupError.style.display = "block";
+        error = true;
+    }
+    if (!regexpDeviceType.test(deviceType.value)) {
+        deviceTypeError.style.display = "block";
+        error = true;
+    }
+    if (error) return;
+
+    const response = getDevices(
+        username.value,
+        password.value,
+        group.value,
+        deviceType.value
+    );
+
+    response.then((dataJson) => {
+        console.log("DATA RECIBIDA DESDE BACKEND UAAP: ");
+        sigfoxResponse = dataJson;
+        console.log(dataJson);
+        pintarData(dataJson);
+        if (dataJson.data.length != 0)
+            unsubscribeButton.style.display = "inline-block";
+        else unsubscribeButton.style.display = "none";
+        loadingData(false);
+    });
+});
+
+//cuando se hace click en el boton Unsubscribe All
+unsubscribeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!sigfoxResponse.hasOwnProperty("data")) {
+        unsubscribeError.textContent = "No devices to unsubscribe!";
+        unsubscribeError.style.display = "block";
+        return;
+    }
+
+    //crear request put al backend uapp para desuscribir
+    console.log("rutina para desuscribir");
+});
